4f9333880f3ca4e9d34930b6b9fa4c9e5a5d3d20
ArkejGit <robert.krawczyk92@gmail.com>
2018-11-26 19:28:10 +0100

    Create User model

---
+++ models/User.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+mongoose.model('User', UserSchema);
