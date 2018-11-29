a604af6e9ff1abbd5fc848a1bf1f067ae1a820cd
ArkejGit <robert.krawczyk92@gmail.com>
2018-11-29 19:24:27 +0100

    Add JWR_SECRET const to config

--- config.js
+++ config.js
@@ -5,4 +5,5 @@ module.exports = {
   PORT: process.env.PORT || 3000,
   URL: process.env.BASE_URL || 'http://localhost:3000',
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://arkej123:arkej123@ds253243.mlab.com:53243/restify-crud',
+  JWT_SECRET: process.env.JWT_SECRET || 'dev_secret',
 };
