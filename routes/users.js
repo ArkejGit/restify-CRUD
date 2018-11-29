2d5b530358533c5fd7322f34a02226d94fb3fd76
ArkejGit <robert.krawczyk92@gmail.com>
2018-11-29 19:05:31 +0100

    Create POST/auth route

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,10 @@
 
 const errors = require('restify-errors');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const auth = require('../auth');
+const config = require('../config');
 
 module.exports = (server) => {
   server.post('/register', (req, res, next) => {
@@ -25,5 +28,25 @@ module.exports = (server) => {
         }
       });
     });
+  });
+
+  server.post('/auth', async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+      const user = await auth.authenticate(email, password);
+
+      const token = jwt.sign(user.toJSON(), config.JWT_SECRET, {
+        expiresIn: '15m',
+      });
+
+      const { iat, exp } = jwt.decode(token);
+
+      res.send({ iat, exp, token });
+
+      next();
+    } catch (err) {
+      return next(new errors.UnauthorizedError(err));
+    }
   });
 };
